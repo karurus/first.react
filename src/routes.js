2b90fbe47bfaaa15c34590943331adede5174a70
wuttipichai karunarat <wu_tipichai555@hotmail.com>
2017-11-19 15:58:49 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Switch, Route , Redirect} from 'react-router-dom'
+
+import Login from './pages/Login'
+import Main from './pages/Main'
+
+const Routes = () => (
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      {!localStorage.getItem('username') ? (
+        <Redirect to="/login" />
+      ) : (
+        <Switch>
+          <Route exact path="/" component={Main} />
+        </Switch>
+      )}
+    </Switch>
+  )
+
+export default Routes
